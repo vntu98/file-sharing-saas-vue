dbb32049a43bff0dd95c6fb1c33147ccc4f18bbb
vntu <vntu98@gmail.com>
2021-07-22 20:00:43 +0700

    login by vuex

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -21,7 +21,14 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-    
+  computed: {
+    ...mapGetters({
+      authenticated: 'auth/authenticated',
+      user: 'auth/user'
+    })
+  }
 }
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    Login
+    <form action="" @submit.prevent="login">
+      <button type="submit">Login</button>
+    </form>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
-  
+  methods: {
+    ...mapActions({
+      loginAction: 'auth/login'
+    }),
+
+    login() {
+      this.loginAction({ email: 'vntu98@gmail.com', password: 'password'})
+    }
+  },
 }
 </script>
