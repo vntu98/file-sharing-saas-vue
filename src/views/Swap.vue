e1bfa243f22e7503db43f5301b74bba3e9bca72e
vntu <vntu98@gmail.com>
2021-07-27 08:55:51 +0700

    screen swap plan

--- src/views/Swap.vue
+++ src/views/Swap.vue
@@ -1,31 +1,65 @@
 <template>
-    <form action="">
+    <form action="" @submit.prevent="swap">
         <div class="mb-6">
-            <div class="mb-2">
-                <app-plan v-for="plan in plans" :key="plan.slug" :plan="plan"/>
+            <div v-for="plan in plans" :key="plan.slug" class="mb-2 flex items-center">
+                <input type="radio" name="plan" :id="`plan_${plan.slug}`" class="mr-3" v-model="form.plan" :value="plan.slug" v-if="availablePlans.find(p => p.slug === plan.slug)">
+                <label :for="`plan_${plan.slug}`" class="flex-grow">
+                    <app-plan :plan="plan"/>
+                </label>
             </div>
         </div>
 
-        <button type="submit" class="bg-indigo-500 text-white px-4 py-3 leading-none rounded-lg font-medium">Swap</button>
+        <button type="submit" class="bg-indigo-500 text-white px-4 py-3 leading-none rounded-lg font-medium" v-if="availablePlans.length">Swap</button>
+        <p v-else class="text-gray-800 text-sm">There are no available plans for you to swap to right now, because your're using too much storage.</p>
     </form>
 </template>
 
 <script>
 import axios from 'axios'
 import AppPlan from '../components/AppPlan.vue'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   components: { AppPlan },
     data() {
         return {
-            plans: []
+            plans: [],
+            planAvailability: [],
+            form: {
+                plan: null
+            }
+        }
+    },
+
+    computed: {
+        ...mapGetters({
+            user: 'auth/user'
+        }),
+
+        availablePlans() {
+            return this.plans.filter(p => p.slug !== this.user.plan.slug && this.planAvailability[p.slug])
         }
     },
 
     async mounted() {
-        let response = await axios.get('/api/plans')
+        let plans = await axios.get('/api/plans')
+        this.plans = plans.data.data
 
-        this.plans = response.data.data
+        let planAvailability = await axios.get('/api/user/plan_availability')
+        this.planAvailability = planAvailability.data.data
+    },
+
+    methods: {
+        ...mapActions({
+            me: 'auth/me'
+        }),
+
+        async swap() {
+            await axios.patch('/api/subscriptions', this.form)
+            await this.me()
+
+            this.$router.replace({ name: 'account' })
+        }
     },
 }
 </script>
